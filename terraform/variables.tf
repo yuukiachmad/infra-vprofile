b5b21682e25b5bb7dd5e9a98960c91c99f55a9b4
yuukiachmad <yuukiachmad@gmail.com>
2026-04-27 17:43:02 +0700

    Updated AWS Region and bucket name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-2"
+  default     = "ap-southeast-3"
 }
 
 variable "clusterName" {
   description = "Name of the EKS cluster"
   type        = string
-  default     = "kitops-eks"
+  default     = "giops-eks"
 }
