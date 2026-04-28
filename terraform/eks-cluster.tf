54c84c58ac484e5115d92748d9769361e7876f26
yuukiachmad <yuukiachmad@gmail.com>
2026-04-28 17:01:51 +0700

    Changed AMI type to AL2023_86_64_STANDART

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -3,14 +3,14 @@ module "eks" {
   version = "19.19.1"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.32"
+  cluster_version = "1.34"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
+    ami_type = "AL2023_x86_64_STANDARD"
 
   }
 
